c079e9697c90c06c93e8a4dff13eb01c7e4111a9
Pedro Paulo <ixf34rxi@gmail.com>
2019-06-29 16:32:10 -0300

    Ingresso não é apagado, mas mostra o nome da pessoa, para apenas 1 pessoa usar

--- confirmar-ingresso.php
+++ confirmar-ingresso.php
@@ -6,13 +6,12 @@ require 'inscricoes.class.php';
 
 $inscricoes = new Inscricoes();
 $eventos = new Eventos(); 
+$usuarios = new Usuarios();
 if(isset($_GET['ingresso']) && !empty($_GET['ingresso'])) {
 	$ingresso = addslashes($_GET['ingresso']);
 
 	if($inscricoes->verificarIngresso($ingresso) == true) {		
-		echo "Ingresso para ".$eventos->getEventoByIngresso($ingresso);
-		$inscricoes->apagarIngresso($ingresso);
-
+		echo "Ingresso para ".$usuarios->getUsuarioByIngresso($ingresso)." em ".$eventos->getEventoByIngresso($ingresso);
 	} else {
 		echo "<h3>Ingresso não existe ou já foi usado!<h3/>";
 	}
